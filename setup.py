d302e03d5163e9e8bfdc8159305433c0695388d1
pbcquoc <pbcquoc@gmail.com>
2020-05-09 16:14:45 +0700

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="transformerocr", # Replace with your own username
+    version="0.1.0",
+    author="pbcquoc",
+    author_email="pbcquoc@gmail.com",
+    description="Transformer base text detection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pbcquoc/transformerocr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
