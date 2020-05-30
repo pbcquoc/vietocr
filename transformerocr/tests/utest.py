c79da98623f0563d9943ae833dc9b152241073e7
pbcquoc <pbcquoc@gmail.com>
2020-05-30 17:44:21 +0700

    test

--- transformerocr/tests/utest.py
+++ transformerocr/tests/utest.py
@@ -10,6 +10,7 @@ def test_loader():
     for batch in iterator:
         assert batch['img'].shape[1]==3, 'image must have 3 channels'
         assert batch['img'].shape[2]==32, 'the height must be 32'
+        print(batch['img'].shape, batch['tgt_input'].shape, batch['tgt_output'].shape, batch['tgt_padding_mask'].shape)
 
 if __name__ == '__main__':
     test_loader()
