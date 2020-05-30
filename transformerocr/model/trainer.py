aa39d19f00fe2091171d38304ea7e6e11f49acb6
pbcquoc <pbcquoc@gmail.com>
2020-05-30 23:02:41 +0700

    loss

--- transformerocr/model/trainer.py
+++ transformerocr/model/trainer.py
@@ -60,7 +60,7 @@ class Trainer():
                 loss = self.step(batch)
                 
                 total_loss += loss
-                print(loss)
+                print(total_loss, loss)
                 self.train_losses.append((self.iter, loss))
 
                 if self.iter % self.print_every == self.print_every - 1:
