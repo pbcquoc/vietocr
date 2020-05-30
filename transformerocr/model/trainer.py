79c98a5735ac5f485d0a2c15ff20193bea16a60a
pbcquoc <pbcquoc@gmail.com>
2020-05-30 21:29:38 +0700

    precision

--- transformerocr/model/trainer.py
+++ transformerocr/model/trainer.py
@@ -2,11 +2,14 @@ from transformerocr.optim.optim import ScheduledOptim
 from torch.optim import Adam
 from torch import nn
 from transformerocr.tool.translate import build_model
+from transformerocr.tool.translate import translate
 from transformerocr.tool.utils import download_weights
 from einops import rearrange
 import yaml
 import torch
 from transformerocr.loader.DataLoader import DataGen
+from transformerocr.tool.utils import compute_accuracy
+
 import numpy as np
 import os
 
@@ -51,7 +54,7 @@ class Trainer():
         for epoch in range(self.num_epochs):
             total_loss = 0
             self.epoch = epoch
-            for batch in self.train_gen.gen(self.batch_size):
+            for batch in self.train_gen.gen(self.batch_size, last_batch=False):
                 self.iter += 1
 
                 loss = self.step(batch)
@@ -60,13 +63,17 @@ class Trainer():
                 self.train_losses.append((self.iter, loss))
 
                 if self.iter % self.print_every == self.print_every - 1:
-                    info = 'iter: {} - epoch: {} - train loss: {:.4f}'.format(self.iter, epoch, total_loss/self.print_every)
+                    info = 'iter: {:06d} - epoch: {:03d} - train loss: {:.4f}'.format(self.iter, epoch, total_loss/self.print_every)
                     total_loss = 0
                     print(info) 
                 
                 if self.valid_annotation and self.iter % self.valid_every == self.valid_every - 1:
                     val_loss = self.validate()
-                    info = 'iter: {} - epoch: {} - val loss: {:.4f}'.format(self.iter, epoch, val_loss)
+                    acc_full_seq, acc_per_char = self.precision()
+
+                    info = 'iter: {:06d} - epoch: {:03d} - val loss: {:.4f} - acc full seq: {:.4f} - acc per char: {:.4f}'.format(
+                            self.iter, epoch, val_loss, 
+                            acc_full_seq, acc_per_char)
                     print(info)
                     self.save_checkpoint(self.checkpoint)
                     self.save_weight(self.export_weights)
@@ -94,6 +101,26 @@ class Trainer():
         self.model.train()
         
         return total_loss
+    
+    def precision(self):
+        pred_sents = []
+	actual_sents = []
+	img_files = []
+
+	for batch in  self.valid_gen.gen(self.batch_size):
+	    translated_sentence = translate(batch['img'], self.model)
+	    pred_sent = self.vocab.batch_decode(translated_sentence.tolist())
+	    actual_sent = self.vocab.batch_decode(batch['tgt_input'].T.tolist())
+
+	    img_files.extend(batch['filenames'])
+
+	    pred_sents.extend(pred_sent)
+	    actual_sents.extend(actual_sent)
+	
+	acc_full_seq = compute_accuracy(actual_sents, pred_sents, mode='full_sequence')
+	acc_per_char = compute_accuracy(actual_sents, pred_sents, mode='per_char')
+    
+        return acc_full_seq, acc_per_char
 
     def load_checkpoint(self, filename):
         checkpoint = torch.load(filename)
--- transformerocr/tool/utils.py
+++ transformerocr/tool/utils.py
@@ -13,3 +13,58 @@ def download_config(id):
         config = yaml.safe_load(f)
 
     return config
+
+def compute_accuracy(ground_truth, predictions, mode='full_sequence'):
+    """
+    Computes accuracy
+    :param ground_truth:
+    :param predictions:
+    :param display: Whether to print values to stdout
+    :param mode: if 'per_char' is selected then
+                 single_label_accuracy = correct_predicted_char_nums_of_single_sample / single_label_char_nums
+                 avg_label_accuracy = sum(single_label_accuracy) / label_nums
+                 if 'full_sequence' is selected then
+                 single_label_accuracy = 1 if the prediction result is exactly the same as label else 0
+                 avg_label_accuracy = sum(single_label_accuracy) / label_nums
+    :return: avg_label_accuracy
+    """
+    if mode == 'per_char':
+
+        accuracy = []
+
+        for index, label in enumerate(ground_truth):
+            prediction = predictions[index]
+            total_count = len(label)
+            correct_count = 0
+            try:
+                for i, tmp in enumerate(label):
+                    if tmp == prediction[i]:
+                        correct_count += 1
+            except IndexError:
+                continue
+            finally:
+                try:
+                    accuracy.append(correct_count / total_count)
+                except ZeroDivisionError:
+                    if len(prediction) == 0:
+                        accuracy.append(1)
+                    else:
+                        accuracy.append(0)
+        avg_accuracy = np.mean(np.array(accuracy).astype(np.float32), axis=0)
+    elif mode == 'full_sequence':
+        try:
+            correct_count = 0
+            for index, label in enumerate(ground_truth):
+                prediction = predictions[index]
+                if prediction == label:
+                    correct_count += 1
+            avg_accuracy = correct_count / len(ground_truth)
+        except ZeroDivisionError:
+            if not predictions:
+                avg_accuracy = 1
+            else:
+                avg_accuracy = 0
+    else:
+        raise NotImplementedError('Other accuracy compute mode has not been implemented')
+
+    return avg_accuracy
