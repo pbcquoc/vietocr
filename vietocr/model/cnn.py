29df27ec0dd29a70482c7451e76f63c28b477a27
pbcquoc <pbcquoc@gmail.com>
2020-06-29 11:00:09 +0700

    cnn

--- vietocr/model/cnn.py
+++ vietocr/model/cnn.py
@@ -1,13 +1,13 @@
 import torch
 from torch import nn
 
-from vietocr.model.vgg import CNN
+from vietocr.model.vgg import Vgg
 from vietocr.model.resnet import Resnet50
 
 class CNN(nn.Module):
     def __init__(self, backbone, *args, **kwargs):
         if backbone == 'vgg':
-            self.model = CNN(*args, **kwargs)
+            self.model = Vgg(*args, **kwargs)
         elif backbone == 'resnet':
             self.model = Resnet50(*args, **kwargs)
 
