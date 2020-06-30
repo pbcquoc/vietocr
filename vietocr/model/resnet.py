f39e88398a474f57692d2d740dbb60dacd1cddd8
pbcquoc <pbcquoc@gmail.com>
2020-06-30 16:46:49 +0700

    resnet

--- vietocr/model/resnet.py
+++ vietocr/model/resnet.py
@@ -3,6 +3,9 @@ from torch import nn
 from torchvision.models import resnet50
 from torchvision.models.resnet import Bottleneck
 from einops import rearrange
+from torch.hub import load_state_dict_from_url
+
+resnet50_url = 'https://download.pytorch.org/models/resnet50-19c8e357.pth'
 
 class Resnet50(nn.Module):
     def __init__(self, hidden=256):
@@ -18,12 +21,16 @@ class Resnet50(nn.Module):
 
         self.layer1 = self.backbone._make_layer(block, 64, layers[0])
         self.layer2 = self.backbone._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self.backbone._make_layer(block, 256, layers[2], stride=2)
+        self.layer3 = self.backbone._make_layer(block, 256, layers[2], stride=(2,1))
         self.layer4 = self.backbone._make_layer(block, 512, layers[3], stride=(2,1))
 
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=(1, 1), padding=1)
         self.conv = nn.Conv2d(2048, hidden, 1)
-
+        
+        state_dict = load_state_dict_from_url(resnet50_url,
+                                              progress=True)
+        self.backbone.load_state_dict(state_dict)
+        
     def forward(self, inputs):
         x = self.backbone.conv1(inputs)
         x = self.backbone.bn1(x)
