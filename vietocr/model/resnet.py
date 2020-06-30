5f54b5303877db4b822cc7899c40ce7b970a38f3
pbcquoc <pbcquoc@gmail.com>
2020-06-30 13:09:55 +0700

    resnet

--- vietocr/model/resnet.py
+++ vietocr/model/resnet.py
@@ -6,11 +6,13 @@ class Resnet50(nn.Module):
     def __init__(self, hidden=256):
         super().__init__()
         self.backbone = resnet50(pretrained=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=(2, 1), padding=1)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=(2, 1), padding=3,
+                                               bias=False)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=(1, 1), padding=1)
         self.conv = nn.Conv2d(2048, hidden, 1)
         
     def forward(self, inputs):
-        x = self.backbone.conv1(inputs)
+        x = self.conv1(inputs)
         x = self.backbone.bn1(x)
         x = self.backbone.relu(x)
         x = self.maxpool(x)
