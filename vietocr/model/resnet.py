bba050cefc3437cce169b13f9b4c0abe5d0b740a
pbcquoc <pbcquoc@gmail.com>
2020-06-30 14:02:21 +0700

    resnet

--- vietocr/model/resnet.py
+++ vietocr/model/resnet.py
@@ -1,27 +1,40 @@
 import torch
 from torch import nn
 from torchvision.models import resnet50
+from torchvision.models.resnet import Bottleneck
 
 class Resnet50(nn.Module):
     def __init__(self, hidden=256):
         super().__init__()
         self.backbone = resnet50(pretrained=True)
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=(2, 1), padding=3,
+        self.backbone.inplanes = 64
+        
+        layers = [3, 4, 6, 3]
+        block = Bottleneck
+        
+        self.conv1 = nn.Conv2d(3, self.backbone.inplanes, kernel_size=7, stride=(2, 2), padding=3,
                                                bias=False)
+        
+        self.layer1 = self.backbone._make_layer(block, 64, layers[0])
+        self.layer2 = self.backbone._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self.backbone._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self.backbone._make_layer(block, 512, layers[3], stride=(2,1))
+        
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=(1, 1), padding=1)
         self.conv = nn.Conv2d(2048, hidden, 1)
-        
+
     def forward(self, inputs):
-        x = self.conv1(inputs)
+        x = self.backbone.conv1(inputs)
         x = self.backbone.bn1(x)
         x = self.backbone.relu(x)
         x = self.maxpool(x)
 
-        x = self.backbone.layer1(x)
-        x = self.backbone.layer2(x)
-        x = self.backbone.layer3(x)
-        x = self.backbone.layer4(x)
-        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
         h = self.conv(x)
         output = h.flatten(2).permute(2, 0, 1)
         return output
+
