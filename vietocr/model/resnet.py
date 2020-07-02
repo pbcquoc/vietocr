4afaa5439b1566a2ab240c4d4d50a8011b3eb7cb
pbcquoc <pbcquoc@gmail.com>
2020-07-02 17:00:41 +0700

    resnet

--- vietocr/model/resnet.py
+++ vietocr/model/resnet.py
@@ -8,7 +8,7 @@ from torch.hub import load_state_dict_from_url
 resnet50_url = 'https://download.pytorch.org/models/resnet50-19c8e357.pth'
 
 class Resnet50(nn.Module):
-    def __init__(self, hidden=256):
+    def __init__(self, ss, hidden=256):
         super().__init__()
         self.backbone = resnet50(pretrained=False)
         self.backbone.inplanes = 64
@@ -16,13 +16,13 @@ class Resnet50(nn.Module):
         layers = [3, 4, 6, 3]
         block = Bottleneck
 
-        self.conv1 = nn.Conv2d(3, self.backbone.inplanes, kernel_size=7, stride=(2, 2), padding=3,
+        self.conv1 = nn.Conv2d(3, self.backbone.inplanes, kernel_size=7, stride=ss[0], padding=3,
                                                bias=False)
 
-        self.backbone.layer1 = self.backbone._make_layer(block, 64, layers[0])
-        self.backbone.layer2 = self.backbone._make_layer(block, 128, layers[1], stride=2)
-        self.backbone.layer3 = self.backbone._make_layer(block, 256, layers[2], stride=2)
-        self.backbone.layer4 = self.backbone._make_layer(block, 512, layers[3], stride=(2,1))
+        self.backbone.layer1 = self.backbone._make_layer(block, 64, layers[0], stride=ss[1])
+        self.backbone.layer2 = self.backbone._make_layer(block, 128, layers[1], stride=ss[2])
+        self.backbone.layer3 = self.backbone._make_layer(block, 256, layers[2], stride=ss[3])
+        self.backbone.layer4 = self.backbone._make_layer(block, 512, layers[3], stride=ss[4])
 
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=(1, 1), padding=1)
         self.conv = nn.Conv2d(2048, hidden, 1)
