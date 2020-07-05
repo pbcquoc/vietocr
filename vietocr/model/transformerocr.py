b7e20435aecf3beeb10b725a0feae4ba99593062
pbcquoc <pbcquoc@gmail.com>
2020-07-05 10:09:35 +0700

    bg

--- vietocr/model/transformerocr.py
+++ vietocr/model/transformerocr.py
@@ -7,21 +7,9 @@ class VietOCR(nn.Module):
                  backbone,
                  cnn_args, 
                  transformer_args):
-#                 ss=[(2,1), (2,1), (2,1), (2,1), (2,1)], 
-#                 ks=[(2,1), (2,1), (1,1), (1,1), (1,1)], 
-#                 d_model=512, nhead=8, 
-#                 num_encoder_layers=6, num_decoder_layers=6, 
-#                 dim_feedforward=2048, max_seq_length=10000, 
-#                 pos_dropout=0.1, trans_dropout=0.1):
         
         super(VietOCR, self).__init__()
         
-        
-#        self.cnn = CNN(**cnn_args)
-#        self.transformer=LanguageTransformer(vocab_size, d_model, nhead, num_encoder_layers,
-#                                num_decoder_layers, dim_feedforward, max_seq_length,
-#                                pos_dropout, trans_dropout)
-
         self.cnn = CNN(backbone, **cnn_args)
         self.transformer=LanguageTransformer(vocab_size, **transformer_args)
 
