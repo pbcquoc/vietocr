410ad58199c5490a6f4f2252d57ff5b6df1773c3
pbcquoc <pbcquoc@gmail.com>
2020-07-12 22:15:41 +0700

    ds

--- vietocr/model/trainer.py
+++ vietocr/model/trainer.py
@@ -48,12 +48,11 @@ class Trainer():
         self.iter = 0
 
         self.optimizer = ScheduledOptim(
-            #Adam(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09),
-            SGD(self.model.parameters()),
+            Adam(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09),
             config['optimizer']['init_lr'], config['transformer']['d_model'], config['optimizer']['n_warmup_steps'])
 
-        self.criterion = nn.CrossEntropyLoss(ignore_index=0) 
-#        self.criterion = LabelSmoothingLoss(len(self.vocab), padding_idx=self.vocab.pad, smoothing=0.1)
+#        self.criterion = nn.CrossEntropyLoss(ignore_index=0) 
+        self.criterion = LabelSmoothingLoss(len(self.vocab), padding_idx=self.vocab.pad, smoothing=0.1)
 
         self.train_gen = self.data_gen('train_db', self.data_root, self.train_annotation)
         if self.valid_annotation != None:
