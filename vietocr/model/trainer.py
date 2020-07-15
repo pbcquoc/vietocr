9b25350521c5d38986774bd9fcdc1f0f9263969c
pbcquoc <pbcquoc@gmail.com>
2020-07-15 11:15:52 +0700

    sgd

--- vietocr/model/trainer.py
+++ vietocr/model/trainer.py
@@ -49,7 +49,8 @@ class Trainer():
         self.iter = 0
 
         self.optimizer = ScheduledOptim(
-            Adam(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09),
+            SGD(self.model.parameters(), lr=0.1, momentum=0.9, nesterov=True),
+#            Adam(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09),
             config['optimizer']['init_lr'], config['transformer']['d_model'], config['optimizer']['n_warmup_steps'])
 
 #        self.criterion = nn.CrossEntropyLoss(ignore_index=0) 
