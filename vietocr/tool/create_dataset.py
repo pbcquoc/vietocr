bac11946461559a26e9402b981d7c224d52cc578
pbcquoc <pbcquoc@gmail.com>
2020-07-18 18:30:29 +0700

    dataset

--- vietocr/tool/create_dataset.py
+++ vietocr/tool/create_dataset.py
@@ -8,10 +8,14 @@ def checkImageIsValid(imageBin):
     if imageBin is None:
         return False
     imageBuf = np.fromstring(imageBin, dtype=np.uint8)
-    img = cv2.imdecode(imageBuf, cv2.IMREAD_GRAYSCALE)
-    imgH, imgW = img.shape[0], img.shape[1]
-    if imgH * imgW == 0:
+    try:
+        img = cv2.imdecode(imageBuf, cv2.IMREAD_GRAYSCALE)
+    except Exception as e:
+        print(e)
         return False
+#    imgH, imgW = img.shape[0], img.shape[1]
+#    if imgH * imgW == 0:
+#        return False
     return True
 
 def writeCache(env, cache):
