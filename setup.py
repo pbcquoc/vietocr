f04c6ff891e53b605665820c062ffe18e90c8ff3
pbcquoc <pbcquoc@gmail.com>
2020-07-28 16:32:11 +0700

    bs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vietocr",
-    version="0.1.7",
+    version="0.1.9",
     author="pbcquoc",
     author_email="pbcquoc@gmail.com",
     description="Transformer base text detection",
