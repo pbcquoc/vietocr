126fa50932ba5ef3468a918a5de63fb107599617
pbcquoc <pbcquoc@gmail.com>
2020-11-13 15:30:47 +0700

    beam search

--- vietocr/tool/translate.py
+++ vietocr/tool/translate.py
@@ -17,7 +17,7 @@ def batch_translate_beam_search(img, model, beam_size=4, candidates=1, max_seq_l
     with torch.no_grad():
         src = model.cnn(img)
         memories = model.transformer.forward_encoder(src)
-        for i in range(memories.size(1)):
+        for i in range(src.size(0)):
 #            memory = memories[:,i,:].repeat(1, beam_size, 1) # TxNxE
             memory = model.transformer.get_memory(memory, i)
             sent = beamsearch(memory, model, device, beam_size, candidates, max_seq_length, sos_token, eos_token)
