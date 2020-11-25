519a5a130fd06887ba07c3102080f03c38fb1538
pbcquoc <pbcquoc@gmail.com>
2020-11-25 10:28:16 +0700

    aug

--- vietocr/loader/aug.py
+++ vietocr/loader/aug.py
@@ -8,17 +8,20 @@ class ImgAugTransform:
   def __init__(self):
     sometimes = lambda aug: iaa.Sometimes(0.5, aug)
 
-    self.aug = iaa.Sequential([
-#        sometimes(iaa.GaussianBlur(sigma=(0, 1.0))),
+    self.aug = iaa.Sequential(iaa.SomeOf((1, None), 
+        [
+        sometimes(iaa.GaussianBlur(sigma=(0, 1.0))),
         sometimes(iaa.MotionBlur(k=3)),
         sometimes(iaa.AddToHueAndSaturation(value=(-10, 10), per_channel=True)),
-#        iaa.PerspectiveTransform(scale=(0.01, 0.15)),
+        sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.15))),
         sometimes(iaa.Affine(scale=(0.7, 1.3), translate_percent=(-0.1, 0.1))),
-#        iaa.PiecewiseAffine(scale=(0.01, 0.01)),
+        sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.01))),
         sometimes(iaa.OneOf([iaa.Dropout(p=(0, 0.1)),
                             iaa.CoarseDropout(p=(0, 0.1), size_percent=(0.02, 0.25))])),
 
-    ])
+    ],
+        random_order=True),
+    random_order=True)
       
   def __call__(self, img):
     img = np.array(img)
