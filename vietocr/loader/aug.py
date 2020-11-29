5e3fb5c6111ffe8da3e20d288f536408fdc3e98c
pbcquoc <pbcquoc@gmail.com>
2020-11-29 11:35:19 +0700

    aug

--- vietocr/loader/aug.py
+++ vietocr/loader/aug.py
@@ -6,13 +6,14 @@ import imgaug as ia
 
 class ImgAugTransform:
   def __init__(self):
-    sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+    sometimes = lambda aug: iaa.Sometimes(0.3, aug)
 
-    self.aug = iaa.Sequential(iaa.SomeOf((1, None), 
+    self.aug = iaa.Sequential(iaa.SomeOf((1, 5), 
         [
         # blur
-        sometimes(iaa.GaussianBlur(sigma=(0, 1.0))),
-        sometimes(iaa.MotionBlur(k=3)),
+
+        sometimes(iaa.OneOf([iaa.GaussianBlur(sigma=(0, 1.0)),
+                            iaa.MotionBlur(k=3)])),
         
         # color
         sometimes(iaa.AddToHueAndSaturation(value=(-10, 10), per_channel=True)),
@@ -23,13 +24,13 @@ class ImgAugTransform:
         sometimes(iaa.Multiply((0.5, 1.5), per_channel=0.5)),
         sometimes(iaa.Add((-40, 40), per_channel=0.5)),
 
-        sometimes(iaa.JpegCompression(compression=(70, 99))),
+        sometimes(iaa.JpegCompression(compression=(5, 80))),
         
         # distort
         sometimes(iaa.Crop(percent=(0.01, 0.05), sample_independently=True)),
         sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.01))),
         sometimes(iaa.Affine(scale=(0.7, 1.3), translate_percent=(-0.1, 0.1), 
-                            rotate=(-5, 5), shear=(-5, 5), 
+#                            rotate=(-5, 5), shear=(-5, 5), 
                             order=[0, 1], cval=(0, 255), 
                             mode=ia.ALL)),
         sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.01))),
