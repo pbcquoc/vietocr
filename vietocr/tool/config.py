488b1d178a4b7a84376628bada583eeb96fcaed2
pbcquoc <pbcquoc@gmail.com>
2021-11-12 14:59:11 +0700

    base config

--- vietocr/tool/config.py
+++ vietocr/tool/config.py
@@ -18,8 +18,8 @@ class Cfg(dict):
 
     @staticmethod
     def load_config_from_file(fname):
-        base_config = download_config(url_config['base'])
-
+        #base_config = download_config(url_config['base'])
+        base_config = {}
         with open(fname, encoding='utf-8') as f:
             config = yaml.safe_load(f)
         base_config.update(config)
