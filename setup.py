335d7ecbef57ac6ffd511111da350faa0de5ae03
Pham Ba Cuong Quoc <pbcquoc@gmail.com>
2022-09-21 17:33:38 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vietocr",
-    version="0.3.8",
+    version="0.3.9",
     author="pbcquoc",
     author_email="pbcquoc@gmail.com",
     description="Transformer base text detection",
