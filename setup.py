30cc6ca9924043523946300d5e4a6379644997be
pbcquoc <pbcquoc@gmail.com>
2022-12-03 10:23:58 +0700

    fix bug download

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vietocr",
-    version="0.3.10",
+    version="0.3.11",
     author="pbcquoc",
     author_email="pbcquoc@gmail.com",
     description="Transformer base text detection",
